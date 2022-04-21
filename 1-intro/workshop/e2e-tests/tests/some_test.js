144659c47f6ca5e483b1d0608674d9146cb5a6cf
Alexandre Sieg <sieg.alexandre@gmail.com>
2022-04-21 11:47:41 +0200

    Finish e2e intro workshop

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -2,4 +2,8 @@ Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+    I.click('.search-cta');
+    I.fillField('.searchfield','info');
+    I.click('.search-input-submit');
+    I.waitForText('Bachelor Informatique');
 });
