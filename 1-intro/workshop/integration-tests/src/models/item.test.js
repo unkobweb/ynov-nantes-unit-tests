647d3e494e94112d1f7f1eabbdb641778cd04071
Alexandre Sieg <sieg.alexandre@gmail.com>
2022-04-21 13:43:28 +0200

    Finish integration tests for Item model

--- 1-intro/workshop/integration-tests/src/models/item.test.js
+++ 1-intro/workshop/integration-tests/src/models/item.test.js
@@ -7,14 +7,22 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
-it("should that create item", async () => {
-  await Item.deleteMany({});
-  let item = new Item({ name: "My super item" });
-  item = await item.save();
-
-  const expectedItem = await Item.findOne({ name: "My super item" });
-  expect(expectedItem.name).toBe(item.name);
-});
+describe("Create item", () => {
+  it("should that create item", async () => {
+    await Item.deleteMany({});
+    let item = new Item({ name: "My super item" });
+    item = await item.save();
+  
+    const expectedItem = await Item.findOne({ name: "My super item" });
+    expect(expectedItem.name).toBe(item.name);
+  });
+  
+  it("should can't create item without name", async () => {
+    await Item.deleteMany({});
+    let item = new Item({});
+    await expect(item.save()).rejects.toThrow('item validation failed: name: Path `name` is required.');
+  });
+})
 
 it("should that return items list", async () => {
   await Item.deleteMany({});
@@ -29,6 +37,40 @@ it("should that return items list", async () => {
   expect(items[1].name).toBe("My super item 2");
 });
 
-// TODO : test update item
+describe("Update item", () => {
+  it("should that update item", async () => {
+    await Item.deleteMany({});
+    await Item.insertMany([
+      { name: "My super item" },
+      { name: "My super item 2" },
+    ]);
+  
+    await Item.updateOne({ name: "My super item" }, { name: "My super item 3" });
+    const items = await Item.find({});
+    expect(items[0].name).toBe("My super item 3");
+    expect(items[1].name).toBe("My super item 2");
+  });
+  
+  it("should can't update item with nont a string name", async () => {
+    await Item.deleteMany({});
+    await Item.insertMany([
+      { name: "My super item" },
+      { name: "My super item 2" },
+    ]);
+  
+    await expect(Item.updateOne({ name: "My super item" }, { name: ['bonjour','test'] })).rejects.toThrow("Cast to string failed for value \"[ 'bonjour', 'test' ]\" (type Array) at path \"name\"");
+  });
+})
 
-// TODO : test delete item
+it("should that delete item", async () => {
+  await Item.deleteMany({});
+  await Item.insertMany([
+    { name: "My super item" },
+    { name: "My super item 2" },
+  ]);
+
+  await Item.deleteOne({ name: "My super item" });
+  const items = await Item.find({});
+  expect(items.length).toBe(1);
+  expect(items[0].name).toBe("My super item 2");
+});
