bc7f8a00e29ae1585a8298f3c2c9bc371edb7fa2
arthurgeay <arthurgeay.contact@gmail.com>
2022-04-28 11:36:34 +0200

    test: check input type

--- 3-test-driven-development/workshop/js/test/minesweeper.test.js
+++ 3-test-driven-development/workshop/js/test/minesweeper.test.js
@@ -4,6 +4,10 @@ const goodField = "3 3\n*..\n...\n.*.\n0 0";
 const badField = "hello world!";
 
 describe("Read input", () => {
+  it("should throw an error if input is not a string", () => {
+    expect(() => new Minesweeper(1)).toThrow("First line must be a string");
+  });
+
   it("Check if first line of input is two numbers separate by a space", () => {
     expect(() => new Minesweeper(badField)).toThrow(
       "First line must be two number separated by a space"
