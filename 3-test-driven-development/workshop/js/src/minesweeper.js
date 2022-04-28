4ded297e63100b1bdc32cd1059ab2f9ffaafe386
arthurgeay <arthurgeay.contact@gmail.com>
2022-04-28 11:10:07 +0200

    feat: throw error when input is bad

--- 3-test-driven-development/workshop/js/src/minesweeper.js
+++ 3-test-driven-development/workshop/js/src/minesweeper.js
@@ -1,5 +1,10 @@
 class Minesweeper {
-  constructor() {}
+  constructor(input) {
+    const lines = input.split("\n");
+    if (lines[0].test(/^[0-9]+\s[0-9]+$/)) {
+      throw new Error("First line must e two number separated by a space");
+    }
+  }
 }
 
 module.exports = { Minesweeper };
