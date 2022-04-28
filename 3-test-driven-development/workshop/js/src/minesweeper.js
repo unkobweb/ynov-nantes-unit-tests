d977c66f094a6af47635d919459da63a0f06bdf3
arthurgeay <arthurgeay.contact@gmail.com>
2022-04-28 11:49:32 +0200

    feat: check if first line is include between 0 and 100

--- 3-test-driven-development/workshop/js/src/minesweeper.js
+++ 3-test-driven-development/workshop/js/src/minesweeper.js
@@ -10,10 +10,18 @@ class Minesweeper {
 
     const lines = input.split("\n");
 
-    const firstLinePattern = new RegExp(/^[0-9]+\s[0-9]+$/, "gm");
+    const firstLinePattern = new RegExp(/^-?[0-9]+\s-?[0-9]+$/, "gm");
 
     if (!firstLinePattern.test(lines[0])) {
       throw new Error("First line must be two number separated by a space");
+    }
+
+    const numbers = lines[0].split(" ").map((nb) => parseInt(nb));
+
+    if (!numbers.every((num) => num > 0 && num <= 100)) {
+      throw new Error(
+        "First line numbers must be greater than 0 and lower or equal than 100"
+      );
     }
   }
 }
